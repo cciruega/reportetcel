f20591770c61f2c1a32c53ba32fffed20ae28551
cciruega <cciruega@gmail.com>
2026-09-17 13:21:02 -0600

    Update portaltelcel.py

--- portaltelcel.py
+++ portaltelcel.py
@@ -291,7 +291,7 @@ if archivo_a_procesar:
                 # Creación ÚNICA de la tabla con todos los formatos visuales (Semáforo y Barra)
                 st.dataframe(
                     df_area.style.format({"Avance": "{:.0%}"}).map(colorear_semaforo, subset=['Avance']),
-                    use_container_width=content,  # <--- CAMBIAR A content
+                    use_container_width=False,  # <--- Regrésalo a esto
                     hide_index=True,
                     height=altura_tabla,
                     column_config={
